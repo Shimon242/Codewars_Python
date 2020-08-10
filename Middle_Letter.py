e659e73139cd9f3eef35de6ba324ab7d6b245209
Shimon Brathwaite <46653837+Shimon242@users.noreply.github.com>
2020-08-10 14:04:17 -0400

    Update Middle_Letter.py

--- Middle_Letter.py
+++ Middle_Letter.py
@@ -1,7 +1,7 @@
 # You are going to be given a word. 
-Your job is to return the middle character of the word. 
-If the word's length is odd, return the middle character. 
-If the word's length is even, return the middle 2 characters.
+# Your job is to return the middle character of the word. 
+# If the word's length is odd, return the middle character. 
+# If the word's length is even, return the middle 2 characters.
 
 def get_middle(s):
     #your code here
@@ -17,7 +17,7 @@ def get_middle(s):
         middle = s[slot]
         return (middle)
 
-Sample Tests
+# Sample Tests
 
 Test.assert_equals(get_middle("test"),"es")
 Test.assert_equals(get_middle("testing"),"t")
